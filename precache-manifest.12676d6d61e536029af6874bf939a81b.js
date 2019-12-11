c64ddd649ea7bf01c9858fb95e23bc0735126f8d
DhirenSc <dhirensc65@gmail.com>
2019-12-10 22:08:27 -0500

    Updates

---
+++ precache-manifest.12676d6d61e536029af6874bf939a81b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb0188c615b8aa8050a47b36a37bfe1c",
+    "url": "/OWMApp/index.html"
+  },
+  {
+    "revision": "73fe70cae5fb68cf06bb",
+    "url": "/OWMApp/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "b3e5d824d31686b1d9ab",
+    "url": "/OWMApp/static/css/main.7a633c89.chunk.css"
+  },
+  {
+    "revision": "73fe70cae5fb68cf06bb",
+    "url": "/OWMApp/static/js/2.f25b539c.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/OWMApp/static/js/2.f25b539c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b3e5d824d31686b1d9ab",
+    "url": "/OWMApp/static/js/main.0db35447.chunk.js"
+  },
+  {
+    "revision": "9e2b5d770f92c7e61740",
+    "url": "/OWMApp/static/js/runtime-main.a9e3e5d0.js"
+  }
+]);
